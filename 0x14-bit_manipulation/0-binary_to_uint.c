c5de762dfdec762a5b6311eecf9c2dcd2f8a423e
kholouudelbatal <kholouud.elbatal@gmail.com>
2023-10-26 19:44:05 -0700

    -binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,18 +7,19 @@
  * Return: he converted number,
  *		or 0 if there is one or more chars in the string
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
 
 	if (!b)
 		return (0);
+	
 	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
 		n = n * 2 + (*b++ - '0');
 	}
+	
 	return (n);
 }
